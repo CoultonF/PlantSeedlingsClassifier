b2c65727fc9f6be86df9ff41897b532d710c219c
Coultonf <cjrfraser@gmail.com>
2018-02-02 03:57:14 +0100

    Initial file structure

---
+++ classifier.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+'''
+images are all square aspect ratio, various dimensions.
+data/
+    train/
+        Black-grass/
+            0a64e3efk.png
+            ...
+        Charlock/
+            0a64e3e6c.png
+            ...
+        Cleavers/
+            7a63e3el9.png
+            ...
+        Common Chickweed/
+            a264e3e6c.png
+            ...
+        Common wheat/
+            0a6fh7e6c.png
+            ...
+        Fat Hen/
+            1a64e3pl5.png
+            ...
+        Loose Silky-bent/
+            ha64e390g.png
+            ...
+        Maize/
+            ja64e3e6c.png
+            ...
+        Scentless Mayweed/
+            0a6fh7e6c.png
+            ...
+        Shepards Purse/
+            6a64e3e6c.png
+            ...
+        Small-flowered Cranesbill/
+            4a64e3e6c.png
+            ...
+        Sugar beet/
+            0a64e3e6c.png
+            ...
+    validation/
+        0a64e3e6c.png
+        0ad9e7dfb.png
+        ...
+'''
+#HYPERPARAMETERS
+filterSize = 3
+batch_size = 256
+epochs = 20
+
+#PREPARE DATA
+
+
+
+#CREATE MODEL
+
+
+
+#COMPILE MODEL
+
+
+
+#TRAIN MODEL
+
+
+
+#TEST MODEL ON VALIDATION DATA
